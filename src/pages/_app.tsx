94cdce6bb3ed454258b3e3972539f8166f3f5760
Jesús DIaz Rivas <jesus20.11@hotmail.es>
2022-06-20 14:18:03 +0200

    [FEAT] GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,14 @@ import "swiper/components/navigation/navigation.scss";
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import TagManager from 'react-gtm-module';
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        TagManager.initialize({ gtmId: 'GTM-K83N7ST' });
+    }, []);
+  
     return (
         <>
         <Head>
